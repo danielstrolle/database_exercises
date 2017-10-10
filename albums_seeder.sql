8a2eb05b009d477e5c12880f92c3ee2d7debc119
danielstrolle <danielstrolle@gmail.com>
2017-10-10 14:04:34 -0500

    Create seeder file that allows us to insert information into our table.
    * create 4 Inserts and apply to our table

---
+++ albums_seeder.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, sales, genre)
+VALUES('Michael Jackson', 'Thriller', '66', 'Pop');
+
+INSERT INTO albums (artist, name, sales, genre)
+VALUES('AC/DC', 'Back in Black', '50', 'Hard Rock');
+
+INSERT INTO albums (artist, name, sales, genre)
+VALUES('Pink Floyd', 'The Dark Side of the Moon', '45', 'Progressive Rock');
+
+INSERT INTO albums (artist, name, sales, genre)
+VALUES('Whitney Houston', 'The Bodyguard', '44', 'R&B');
