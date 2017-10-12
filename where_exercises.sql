30844397a538ed641f596b16954e611590554b35
danielstrolle <danielstrolle@gmail.com>
2017-10-12 09:27:55 -0500

    Complete the where exercise
    * write queries to search through our database for specific instances

---
+++ where_exercises.sql
@@ -0,0 +1,22 @@
+USE employees;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+-- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+where hire_date like '199%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
