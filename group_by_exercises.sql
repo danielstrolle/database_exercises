269e70906e9339e9e7566deb62639de7d4e9c5d1
danielstrolle <danielstrolle@gmail.com>
2017-10-12 14:23:27 -0500

    Complete the group by exercise, sorting my query returns and showing unique instances of the specified parameter

---
+++ group_by_exercises.sql
@@ -0,0 +1,23 @@
+USE employees;
+
+SELECT title
+FROM titles
+GROUP BY title;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E'
+GROUP BY last_name;
+
+
+SELECT DISTINCT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
